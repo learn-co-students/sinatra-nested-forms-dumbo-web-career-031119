2dd548f2531692c8f805431080e4da249dde9c67
github username <itsDario1@gmail.com>
2019-04-03 06:16:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+      # binding.pry
+        @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+        ships = params[:pirate][:ships]
+        # binding.pry
+        ships.each do |ship|
+          Ship.new(ship[:name], ship[:type], ship[:booty])
+          # binding.pry
+        end
+# binding.pry
+        erb :'/pirates/show'
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  @@all = []
+  attr_reader :name, :type, :booty
+  def initialize(name,type,booty) 
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
