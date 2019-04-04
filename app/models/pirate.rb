eefd27b608b23691319caa61d21858b8c6024077
beschwa <naraiswar@gmail.com>
2019-04-04 10:39:03 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+	@@ALL = []
+	attr_accessor :name, :weight, :height
+
+	def initialize(name, weight, height)
+		@name, @weight, @height = name, weight, height
+		@@ALL << self
+	end
+
+	def self.all
+		@@ALL
+	end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+	@@ALL = []
+	attr_accessor :name, :type, :booty
+
+	def initialize(sname, type, booty)
+		@name = sname
+		@type = type
+		@booty = booty
+		@@ALL << self
+	end
+
+	def self.all
+		@@ALL
+	end
+
+	def self.clear
+		@@ALL = []
+	end
+
 end
